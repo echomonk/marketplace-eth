255945a84e18840f9545f7c523ed982076c57e73
echomonk <erdaloylupinar@gmail.com>
2022-03-15 09:11:52 +0300

    Marketplace-ETH

--- components/hooks/web3/index.js
+++ components/hooks/web3/index.js
@@ -1,41 +1,41 @@
-
-import { useHooks } from "@components/providers/web3";
+import { useHooks } from "@components/providers/web3"
 
 const enhanceHook = swrRes => {
-    return {
-       ...swrRes,
-       hasInitialResponse: swrRes.data || swrRes.error 
-    }
+  return {
+    ...swrRes,
+    hasInitialResponse: swrRes.data || swrRes.error
+  }
 }
 
 export const useNetwork = () => {
-    const swrRes = enhanceHook(useHooks(hooks => hooks.useNetwork)())
-    return {
-        network: swrRes
-    }
+  const swrRes = enhanceHook(useHooks(hooks => hooks.useNetwork)())
+  return {
+    network: swrRes
+  }
 }
 
 export const useAccount = () => {
-    const swrRes = enhanceHook(useHooks(hooks => hooks.useAccount)())
-    return {
-        account: swrRes
-    }
+  const swrRes = enhanceHook(useHooks(hooks => hooks.useAccount)())
+  return {
+    account: swrRes
+  }
 }
 
-export const useOwnedCourses = () => {
-    const res = useHooks(hooks => hooks.useOwnedCourses)()
-    return {
-        ownedCourses: { data: res }
-    }
+export const useOwnedCourses = (...args) => {
+  const swrRes = enhanceHook(useHooks(hooks => hooks.useOwnedCourses)(...args))
+
+  return {
+    ownedCourses: swrRes
+  }
 }
 
 export const useWalletInfo = () => {
-    const { account } = useAccount()
-    const { network } = useNetwork()
+  const { account } = useAccount()
+  const { network } = useNetwork()
 
-    return {
-        account,
-        network,
-        canPurchaseCourse: !!(account.data && network.isSupported)
-    }
+  return {
+    account,
+    network,
+    canPurchaseCourse: !!(account.data && network.isSupported)
+  }
 }
--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,32 +1,42 @@
 
 
-import { useOwnedCourses } from "@components/hooks/web3";
+import { useAccount, useOwnedCourses } from "@components/hooks/web3";
 import { Button, Message } from "@components/ui/common";
 import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
+import { getAllCourses } from "@content/courses/fetcher";
 
-export default function OwnedCourses() {
-    const { ownedCourses } = useOwnedCourses()
+export default function OwnedCourses({courses}) {
+  const { account } = useAccount()
+  const { ownedCourses } = useOwnedCourses(courses, account.data)
 
-    return (
-        <>
-            { ownedCourses.data }
-            <div className="py-4">
-                <MarketHeader />
-            </div>
-            <section className="grid grid-cols-1">
-            <OwnedCourseCard>
-                <Message>
-                    My custom message
-                </Message>
-                <Button>
-                    Watch the course
-                </Button>
-            </OwnedCourseCard>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <div className="py-4">
+        <MarketHeader />
+      </div>
+      <section className="grid grid-cols-1">
+        <OwnedCourseCard>
+          <Message>
+            My custom message!
+          </Message>
+          <Button>
+            Watch the course
+          </Button>
+        </OwnedCourseCard>
+      </section>
+    </>
+  )
+}
+
+export function getStaticProps() {
+  const { data } = getAllCourses()
+  return {
+    props: {
+      courses: data
+    }
+  }
 }
 
 OwnedCourses.Layout = BaseLayout
