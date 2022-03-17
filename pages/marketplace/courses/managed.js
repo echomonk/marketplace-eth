1ac4b549aa01d415178aa51a1529a1aa5c5b9715
echomonk <erdaloylupinar@gmail.com>
2022-03-17 11:17:36 +0300

    Marketplace-ETH

--- pages/marketplace/courses/managed.js
+++ pages/marketplace/courses/managed.js
@@ -2,16 +2,36 @@
 
 
 import { useAccount, useManagedCourses } from "@components/hooks/web3";
-import { Button } from "@components/ui/common";
+import { useWeb3 } from "@components/providers";
+import { Button, Message } from "@components/ui/common";
 import { CourseFilter, ManagedCourseCard, OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
+import { useState } from "react";
 
 export default function ManagedCourses() {
+    const [ email, setEmail ] = useState("")
+    const [ proofOwnership, setProofOwnership ] = useState({})
+    const { web3 } = useWeb3()
     const { account } = useAccount()
     const { managedCourses } = useManagedCourses(account.data)
 
-    console.log(managedCourses.data)
+    const verifyCourse = (email, {hash, proof}) => {
+        const emailHash = web3.utils.sha3(email)
+        const proofToCheck = web3.utils.soliditySha3(
+          { type: "bytes32", value: emailHash },
+          { type: "bytes32", value: hash }  
+        )
+
+        proofToCheck === proof ? 
+        setProofOwnership ({
+            [hash]: true
+        }) : 
+        setProofOwnership ({
+            [hash]: false
+        }) 
+    }
+
     return (
         <>
             <MarketHeader />
@@ -19,20 +39,42 @@ export default function ManagedCourses() {
         <section className="grid grid-cols-1">
             { managedCourses.data?.map(course =>
               <ManagedCourseCard
-                key={course.ownedCourseId}
-                course={course}
+                    key={course.ownedCourseId}
+                    course={course}
               >
                   <div className="flex mr-2 relative rounded-md">
                 <input
+                    value={email}
+                    onChange={({target: {value}}) => setEmail(value)}
                     type="text"
                     name="account"
                     id="account"
                     className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
                     placeholder="0x2341ab..." />
-                <Button>
+                <Button 
+                    onClick={() =>{
+                        verifyCourse(email, {
+                            hash: course.hash, 
+                            proof: course.proof})
+                    }}
+                >
                     Verify
                 </Button>
             </div>
+            { proofOwnership[course.hash] &&
+                <div className="mt-2">
+                  <Message>
+                    Verified
+                  </Message>  
+                </div>
+            }
+             { proofOwnership[course.hash] === false &&
+                <div className="mt-2">
+                  <Message type="danger">
+                    Wrong Proof!
+                  </Message>  
+                </div>
+            }    
               </ManagedCourseCard>
             )}
         </section>
