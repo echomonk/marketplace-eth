b09659af774a51cabfaaa45f027e67c61244b72a
echomonk <erdaloylupinar@gmail.com>
2022-03-23 14:38:52 +0300

    Marketplace-ETH

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const keys = require("./keys.json")
 
 
 module.exports = {
@@ -7,6 +9,21 @@ module.exports = {
      host: "127.0.0.1",
      port: 7545,
      network_id: "*",
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: keys.MNEMONIC
+          },
+          providerOrUrl: `https://ropsten.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
+          addressIndex: 0
+        }),
+      network_id: 3,
+      gas: 5500000, // Gas Limit, How much gas we are willing to spent
+      gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200 // number of blocks before deployment times out
     }
   },
   compilers: {
@@ -15,3 +32,8 @@ module.exports = {
     }
   }
 }
+
+// 5500000 * 20000000000 = 110000000000000000 = 0,11 ETH => 326,68 USD
+
+// transaction hash: 0x136e2917b15beb5f44467fe44892a325c6c3ee346bd8d29ae280e7b17211cc35
+// contract address: 0x4Aa288Ab3F9115c23F387D88e811fA436800e77F
