3b37db0854c7fbbbcc2b3e3a9a4a95ad0222ada1
echomonk <erdaloylupinar@gmail.com>
2022-03-25 19:44:39 +0300

    Marketplace-ETH

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -180,8 +180,6 @@ export default function Marketplace({courses}) {
                   </>
                 )
               }
-
-
               return (
                 <Button
                   size = "sm"
