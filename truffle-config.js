3cd940251bfd4be6f3f363a8716d0e60f338935e
echomonk <erdaloylupinar@gmail.com>
2022-03-25 12:08:25 +0300

    Marketplace-ETH

--- truffle-config.js
+++ truffle-config.js
@@ -20,8 +20,8 @@ module.exports = {
           addressIndex: 0
         }),
       network_id: 3,
-      gas: 5500000, // Gas Limit, How much gas we are willing to spent
-      gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+      gas: 5500000, // Gas Limit, How much gas we are willing to spend
+      gasPrice: 20000000000, // how much we are willing to spend for unit of gas
       confirmations: 2, // number of blocks to wait between deployment
       timeoutBlocks: 200 // number of blocks before deployment times out
     }
